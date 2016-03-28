746aacf89a13fd9b57ff32049179a12c7c7bea46
Kevin Graney <nanonet@gmail.com>
2016-03-28 16:45:31 -0400

    Add day1 part 2

--- day1.hs
+++ day1.hs
@@ -1,10 +1,19 @@
-parseSequence floor string =
+-- Part 1
+finalFloor floor string =
     case string of
-        ('(':t) -> parseSequence (floor + 1) t
-        (')':t) -> parseSequence (floor - 1) t
+        ('(':t) -> finalFloor (floor + 1) t
+        (')':t) -> finalFloor (floor - 1) t
         [] -> floor
+
+-- Part 2
+enterBasement (-1) count string = count
+enterBasement floor count string =
+    case string of
+        ('(':t) -> enterBasement (floor + 1) (count + 1) t
+        (')':t) -> enterBasement (floor - 1) (count + 1) t
+        [] -> -1
 
 main = do
     s <- getLine
-    let floor = parseSequence 0 s
-    print floor 
+    print $ finalFloor 0 s
+    print $ enterBasement 0 0 s
