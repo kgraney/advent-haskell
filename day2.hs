5ca8f531c47ab72e464b20626d5f4a80a4ab2d2d
Kevin Graney <nanonet@gmail.com>
2016-04-04 16:40:54 -0400

    Day 2

---
+++ day2.hs
@@ -0,0 +1,21 @@
+import qualified Data.Text as T
+
+paperSize :: [Integer] -> Integer
+paperSize [l, w, h] = 2*l*w + 2*w*h + 2*h*l + minimum [l*w, w*h, h*l]
+
+ribbonSize :: [Integer] -> Integer
+ribbonSize [l, w, h] = 2 * (minimum [l+w, w+h, h+l]) + l*w*h
+
+parseDims :: String -> [Integer]
+parseDims s = 
+    map read $ map T.unpack $ T.splitOn (T.pack "x") (T.pack s)
+
+compute :: ([Integer] -> Integer) -> [String] -> Integer
+compute f boxes =
+    sum $ map f $ map parseDims boxes
+
+
+main = do
+    s <- getContents
+    print $ compute paperSize $ words s
+    print $ compute ribbonSize $ words s
